8df46490f83195ece5e9e14023785a19e28c484b
Alexander Lapaev <a.lapaev@pixonic.ru>
2018-08-25 09:52:25 +0300

    compose

--- t1.bat
+++ t1.bat
@@ -2,3 +2,9 @@ curl --header "PlayerX: b454207b-ddff-4992-8480-1789990562e8" http://localhost:7
 curl --header "Player: b454207b-ddff-4992-8480-1789990562e8" http://localhost:7080/content.html
 curl --header "Player: 370f2e9c-7936-4e3d-8c91-9d1a01df7b29" http://localhost:7080/content.html
 curl --header "Player: 0fc74244-ba89-44c4-a599-76605ff52d01" http://localhost:7080/content.html
+
+curl --header "PlayerX: b454207b-ddff-4992-8480-1789990562e8" http://localhost:7080/content.html
+curl --header "Player: b454207b-ddff-4992-8480-1789990562e8" http://localhost:7080/content.html
+curl --header "Player: 370f2e9c-7936-4e3d-8c91-9d1a01df7b29" http://localhost:7080/content.html
+curl --header "Player: 0fc74244-ba89-44c4-a599-76605ff52d01" http://localhost:7080/content.html
+
